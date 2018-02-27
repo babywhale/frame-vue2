d31e31ee3a0d9053e1c49f678752e2b3c1ccd47d
babywhale <xjniu1990@163.com>
2018-02-27 11:28:55 +0800

    add global stylus

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,10 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus">
+@import './common/global.styl'
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
