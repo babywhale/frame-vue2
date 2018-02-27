dbc07b5fd5a02e139447acfbaa3e9699687f93eb
babywhale <xjniu1990@163.com>
2018-02-27 10:10:16 +0800

    add stylus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,7 +95,8 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="stylus">
+@import './testStyle.styl'
 h1, h2 {
   font-weight: normal;
 }
@@ -106,8 +107,5 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
